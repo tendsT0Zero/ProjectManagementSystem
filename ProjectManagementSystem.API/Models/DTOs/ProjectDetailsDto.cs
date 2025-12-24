e3dfad1af7d589c1a265bda9a96c75b3d9b47107
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 15:39:47 +0600

    Added ProjectDetailsDto class for project details data

---
+++ ProjectManagementSystem.API/Models/DTOs/ProjectDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectManagementSystem.API.Models.DTOs
+{
+    public class ProjectDetailsDto
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public string TeamLeaderName { get; set; } = string.Empty;
+        public List<string> MemberNames { get; set; } = new();
+    }
+}
