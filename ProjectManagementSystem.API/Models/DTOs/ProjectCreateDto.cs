a5c72e1d3312115b70e9719b247093b5dc00d16c
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 09:53:05 +0600

    Project Create dto to fetch request data from user

---
+++ ProjectManagementSystem.API/Models/DTOs/ProjectCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectManagementSystem.API.Models.DTOs
+{
+    public class ProjectCreateDto
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Deadline { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string TeamLeaderId { get; set; }
+
+        [ForeignKey("TeamLeaderId")]
+        public ApplicationUser TeamLeader { get; set; }
+        public ICollection<ProjectMember> Members { get; set; }
+        public ICollection<TaskItem> Tasks { get; set; }
+    }
+}
