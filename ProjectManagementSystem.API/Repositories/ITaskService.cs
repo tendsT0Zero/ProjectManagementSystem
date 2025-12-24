5a6aa29280578cb955f5cad716bb2b67cf005519
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 15:33:56 +0600

    Added ITaskService interface for task management operations

---
+++ ProjectManagementSystem.API/Repositories/ITaskService.cs
@@ -0,0 +1,16 @@
+﻿using ProjectManagementSystem.API.Models;
+using ProjectManagementSystem.API.Models.DTOs;
+
+namespace ProjectManagementSystem.API.Repositories
+{
+    public interface ITaskService
+    {
+        // create task and assign to team member on a specific project
+        // Post: api/ProjectManager/{projectId}/create-task/
+        Task<ResponseDto> CreateTaskAsync(int projectId, TaskItem task);
+
+        Task<ResponseDto> UpdateTaskAsync(int id, TaskItem task);
+
+        Task<ResponseDto> GetTasksByProjectIdAsync(int projectId);
+    }
+}
