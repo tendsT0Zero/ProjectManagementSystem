cbe78ad3f79a419249a333236208715b8b412bca
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 15:39:21 +0600

    Added UpdateTaskItemDto for updating task items

---
+++ ProjectManagementSystem.API/Models/DTOs/UpdateTaskItemDto.cs
@@ -0,0 +1,24 @@
+﻿using ProjectManagementSystem.API.Static_Details;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectManagementSystem.API.Models.DTOs
+{
+    public class UpdateTaskItemDto
+    {
+
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+        [Required]
+        public string? Status { get; set; } = SD.TaskStatus.Pending.ToString();
+        public string? Priority { get; set; } = SD.TaskPriority.Low.ToString();
+        public DateTime? DueDate { get; set; }
+
+        //Assigned User
+        public string AssignedToUserId { get; set; }
+
+    }
+}
