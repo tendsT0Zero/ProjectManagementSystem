811868f6d03fa8952597b5ec7175d21d85aa5ae8
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 12:13:54 +0600

    Update ProjectCreateDto: make fields nullable, clean up props

--- ProjectManagementSystem.API/Models/DTOs/ProjectCreateDto.cs
+++ ProjectManagementSystem.API/Models/DTOs/ProjectCreateDto.cs
@@ -5,21 +5,21 @@ namespace ProjectManagementSystem.API.Models.DTOs
 {
     public class ProjectCreateDto
     {
+
+
         [Required]
         [MaxLength(150)]
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public DateTime Deadline { get; set; }
+        public DateTime? Deadline { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         [Required]
-        public string TeamLeaderId { get; set; }
+        public string? TeamLeaderId { get; set; }
 
-        [ForeignKey("TeamLeaderId")]
-        public ApplicationUser TeamLeader { get; set; }
-        public ICollection<ProjectMember> Members { get; set; }
-        public ICollection<TaskItem> Tasks { get; set; }
+        public ICollection<string>? MembersIds { get; set; }
+
     }
 }
