a174e6fa7a7f58b95c607d4f89226823404e9f4c
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 15:35:52 +0600

    Added IProjectMemberService interface with member management methods

---
+++ ProjectManagementSystem.API/Repositories/IProjectMemberService.cs
@@ -0,0 +1,10 @@
+﻿using ProjectManagementSystem.API.Models.DTOs;
+
+namespace ProjectManagementSystem.API.Repositories
+{
+    public interface IProjectMemberService
+    {
+        Task<ResponseDto> UpdateProjectMembersAsync(int projectId, UpdateProjectMembersDto updateProjectMembersDto);
+        Task<ResponseDto> AddNewMemberToProjectAsync(int projectId, string memberId);
+    }
+}
