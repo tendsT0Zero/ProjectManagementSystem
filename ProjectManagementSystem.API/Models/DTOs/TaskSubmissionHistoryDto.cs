628fa9c6cf2c910cfdb481bcf1399d7ffc11942d
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-24 16:54:08 +0600

    Added TaskSubmissionHistoryDto for task submission details

---
+++ ProjectManagementSystem.API/Models/DTOs/TaskSubmissionHistoryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectManagementSystem.API.Models.DTOs
+{
+    public class TaskSubmissionHistoryDto
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public string TaskTitle { get; set; } = string.Empty;
+        public string TaskDescription { get; set; }
+
+        public DateOnly SubmissionDate { get; set; }
+        public string SubmitterName { get; set; } = string.Empty;
+        public string SubmissionNotes { get; set; }
+        public string SubmissionAttachmentUrl { get; set; }
+    }
+}
