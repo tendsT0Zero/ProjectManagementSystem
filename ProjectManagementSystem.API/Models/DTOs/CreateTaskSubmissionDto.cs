a7e23d77d1f2d93da7cab7b0370bdc14c50ed5d1
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-25 10:12:48 +0600

    DTO to fetch task submission related data from user.

---
+++ ProjectManagementSystem.API/Models/DTOs/CreateTaskSubmissionDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectManagementSystem.API.Models.DTOs
+{
+    public class CreateTaskSubmissionDto
+    {
+        public string SubmissionNotes { get; set; }
+        public IFormFile? File { get; set; }
+
+        // Task
+        public int TaskId { get; set; }
+    }
+}
